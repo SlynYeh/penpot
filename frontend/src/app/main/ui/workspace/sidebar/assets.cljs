1e9a0f086d060b4c1f7d96240272c0dd66e8cf7c
koalaofpoint <angelwudan@126.com>
2026-09-07 18:14:47 +0800

    feat:调整管理库和搜索框样式

--- frontend/src/app/main/ui/workspace/sidebar/assets.cljs
+++ frontend/src/app/main/ui/workspace/sidebar/assets.cljs
@@ -18,7 +18,7 @@
    [app.main.ui.components.search-bar :refer [search-bar*]]
    [app.main.ui.context :as ctx]
    [app.main.ui.ds.buttons.icon-button :refer [icon-button*]]
-   [app.main.ui.icons :as deprecated-icon]
+   [app.main.ui.ds.foundations.assets.icon :as i]
    [app.main.ui.workspace.sidebar.assets.common :as cmm]
    [app.main.ui.workspace.sidebar.assets.file-library :refer [file-library*]]
    [app.util.dom :as dom]
@@ -173,46 +173,37 @@
 
     [:article  {:class (stl/css :assets-bar)}
      [:div {:class (stl/css :assets-header)}
-      (when-not ^boolean read-only?
-        (if (and (= num-libs 1) (empty? components) (not shared?))
-          [:button {:class (stl/css :add-library-button)
-                    :on-click show-libraries-dialog
-                    :data-testid "libraries"}
-           (tr "workspace.assets.add-library")]
-
-          [:button {:class (stl/css :libraries-button)
-                    :on-click show-libraries-dialog
-                    :data-testid "libraries"}
-           (tr "workspace.assets.manage-library")]))
-
-
       [:div {:class (stl/css :search-wrapper)}
        [:> search-bar* {:on-change on-search-term-change
                         :value term
-                        :placeholder (tr "workspace.assets.search")}
-        [:button
-         {:on-click on-open-menu
-          :title (tr "workspace.assets.filter")
-          :class (stl/css-case :section-button true
-                               :opened menu-open?)}
-         deprecated-icon/filter-icon]]
+                        :placeholder (tr "workspace.assets.search")}]]
 
-       [:> context-menu*
-        {:on-close on-menu-close
-         :selectable true
-         :selected section
-         :show menu-open?
-         :fixed true
-         :min-width true
-         :width size
-         :top 158
-         :left 18
-         :options options}]
+      (when-not ^boolean read-only?
+        [:div {:class (stl/css :actions)}
+         [:> icon-button* {:variant (if menu-open? "secondary" "ghost")
+                           :icon i/filter
+                           :aria-label (tr "workspace.assets.filter")
+                           :tooltip-placement "top"
+                           :on-click on-open-menu}]
+         [:> icon-button* {:variant "ghost"
+                           :icon i/book-open
+                           :aria-label (tr "workspace.assets.manage-library")
+                           :tooltip-placement "top"
+                           :on-click show-libraries-dialog
+                           :data-testid "libraries"}]])
 
-       [:> icon-button* {:variant "ghost"
-                         :aria-label (tr "workspace.assets.sort")
-                         :on-click toggle-ordering
-                         :icon (if reverse-sort? "asc-sort" "desc-sort")}]]]
+      [:> context-menu*
+       {:on-close on-menu-close
+        :selectable true
+        :selected section
+        :show menu-open?
+        :fixed true
+        :min-width true
+        :width size
+        :top 158
+        :left 18
+        :options options}]]
+
 
      [:& (mf/provider cmm/assets-filters) {:value filters}
       [:& (mf/provider cmm/assets-toggle-ordering) {:value toggle-ordering}
